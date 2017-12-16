096d38e6ab83a14051b65bcf71218eaf43dc6389
Mohammed Niyas <Mohammed Niyas>
2017-12-16 00:39:05 +0000

    added url encoding

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var mongoose = require('mongoose');
 Geners = require('./models/genere');
 Books = require('./models/book');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 //connect to mongoose
 mongoose.connect('mongodb://127.0.0.1:27017/bookstore');
 var db = mongoose.Connection;
